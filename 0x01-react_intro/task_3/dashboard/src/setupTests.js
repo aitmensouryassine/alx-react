e5712d1917a43c1cd62477c297a0f5ed56df7d56
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2024-01-30 23:44:15 +0100

    Tests

--- 0x01-react_intro/task_3/dashboard/src/setupTests.js
+++ 0x01-react_intro/task_3/dashboard/src/setupTests.js
@@ -1,3 +1,4 @@
-import { configure, EnzymeAdapter } from 'enzyme';
+import { configure } from 'enzyme';
+import Adapter from '@cfaester/enzyme-adapter-react-18';
 
-configure({ adapter: new EnzymeAdapter() });
+configure({ adapter: new Adapter() });

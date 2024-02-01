f60a6100e2d58cc55eb55927ef6087b4bb628327
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2024-02-01 23:48:56 +0100

    Task 3

--- 0x02-react_props/task_2/dashboard/src/Notifications/NotificationItem.js
+++ 0x02-react_props/task_2/dashboard/src/Notifications/NotificationItem.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 export default function NotificationItem({ type, html, value }) {
 	return html ? (
-		<li data-notification-type={type} dangerouslySetInnerHTML={html}></li>
+		<li data-priority={type} data-notification-type={type} dangerouslySetInnerHTML={html}></li>
 	) : (
-		<li data-notification-type={type}>{value}</li>
+		<li data-priority={type} data-notification-type={type}>
+			{value}
+		</li>
 	);
 }

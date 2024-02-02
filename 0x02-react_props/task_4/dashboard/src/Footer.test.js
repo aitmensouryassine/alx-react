082219a20991b9a478b133b425679009c764c566
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2024-02-02 18:27:08 +0100

    Css fix

---
+++ 0x02-react_props/task_4/dashboard/src/Footer.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from './Footer/Footer';
+import { shallow } from 'enzyme';
+
+describe('<Footer />', () => {
+	it('Footer renders without crashing', () => {
+		shallow(<Footer />);
+	});
+	it('Footer at the very least render the text “Copyright”', () => {
+		const footer = shallow(<Footer />);
+		const text = footer.text();
+		expect(text).toContain('Copyright');
+	});
+});

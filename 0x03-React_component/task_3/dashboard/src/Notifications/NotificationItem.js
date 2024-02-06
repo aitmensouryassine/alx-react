a163f4bad94c0fbb1bc6764f3c9c421cf5183078
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2024-02-06 16:39:21 +0100

    Task 3

---
+++ 0x03-React_component/task_3/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class NotificationItem extends Component {
+	render() {
+		return this.props.html ? (
+			<li
+				data-notification-type={this.props.type}
+				dangerouslySetInnerHTML={this.props.html}
+				onClick={() => this.props.markAsRead(this.props.id)}
+			></li>
+		) : (
+			<li data-notification-type={this.props.type} onClick={() => this.props.markAsRead(this.props.id)}>
+				{this.props.value}
+			</li>
+		);
+	}
+}
+
+NotificationItem.defaultProps = {
+	type: 'default',
+	value: '',
+	markAsRead: () => {},
+};
+NotificationItem.propType = {
+	html: PropTypes.shape({
+		__html: PropTypes.string,
+	}),
+	type: PropTypes.string.isRequired,
+	value: PropTypes.string,
+	markAsRead: PropTypes.func,
+};
+
+export default NotificationItem;

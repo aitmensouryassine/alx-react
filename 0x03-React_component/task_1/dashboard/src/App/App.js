08ec1b8d8a2d16f4e77bf4b80278f8b13080bda3
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2024-02-08 23:07:04 +0100

    Task 1 Fix

--- 0x03-React_component/task_1/dashboard/src/App/App.js
+++ 0x03-React_component/task_1/dashboard/src/App/App.js
@@ -8,34 +8,22 @@ import Notifications from '../Notifications/Notifications';
 import CourseList from '../CourseList/CourseList';
 import { getLatestNotification } from '../utils/utils';
 
-const listCourses = [
-	{ id: 1, name: 'ES6', credit: 60 },
-	{ id: 2, name: 'Webpack', credit: 20 },
-	{ id: 3, name: 'React', credit: 40 },
-];
-const listNotifications = [
-	{
-		id: 0,
-		type: 'default',
-		value: 'New course available',
-	},
-	{
-		id: 1,
-		type: 'urgent',
-		value: 'New resume available',
-	},
-	{
-		id: 2,
-		type: 'urgent',
-		html: { __html: getLatestNotification() },
-	},
-];
-
 class App extends Component {
 	constructor(props) {
 		super(props);
 		this.handleLogOut = this.handleLogOut.bind(this);
 	}
+
+	listCourses = [
+		{ id: 1, name: 'ES6', credit: 60 },
+		{ id: 2, name: 'Webpack', credit: 20 },
+		{ id: 3, name: 'React', credit: 40 },
+	];
+	listNotifications = [
+		{ id: 1, type: 'default', value: 'New course available' },
+		{ id: 2, type: 'urgent', value: 'New resume available' },
+		{ id: 3, type: 'urgent', html: { __html: getLatestNotification() } },
+	];
 
 	handleLogOut = (evt) => {
 		if (evt.ctrlKey && evt.key === 'h') {
@@ -56,11 +44,11 @@ class App extends Component {
 	render() {
 		return (
 			<>
-				<Notifications listNotifications={listNotifications} />
+				<Notifications listNotifications={this.listNotifications} />
 				<div className='App'>
 					<Header />
 					<div className='App-body'>
-						{this.props.isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
+						{this.props.isLoggedIn ? <CourseList listCourses={this.listCourses} /> : <Login />}
 					</div>
 					<Footer />
 				</div>

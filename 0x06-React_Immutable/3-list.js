501fbc6826525f33e5803142838ade8b42ac8510
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2024-02-21 20:38:57 +0100

    change

--- 0x06-React_Immutable/3-list.js
+++ 0x06-React_Immutable/3-list.js
@@ -1,6 +1,8 @@
 import { List } from 'immutable';
 
-const getListObject = (array) => List(array);
-const addElementToList = (list, element) => list.push(element);
-
-export { getListObject, addElementToList };
+export function getListObject(array) {
+  return List(array);
+}
+export function addElementToList(list, element) {
+  return list.push(element);
+}
